9547e70839f78cd30d7b198a4437db2e5426ef9e
Wentao He <wentaohe@umich.edu>
2026-03-31 16:21:00 -0400

    add: ppo and sac training scripts

---
+++ ece567_scripts/ppo_arm_reach.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 \
+uv run python /data/yhluo/ece567/JaxGCRL/run.py ppo \
+  --env arm_reach \
+  --backend mjx \
+  --total_env_steps 10000000 \
+  --episode_length 1000 \
+  --num_envs 256 \
+  --num_eval_envs 256 \
+  --num_evals 50 \
+  --seed 1 \
+  --learning_rate 3e-4 \
+  --discounting 0.99 \
+  --unroll_length 20 \
+  --batch_size 32 \
+  --num_minibatches 16 \
+  --num_updates_per_batch 4 \
+  --normalize_observations \
+  --normalize_advantage \
+  --clipping_epsilon 0.2 \
+  --gae_lambda 0.95 \
+  --wandb_project_name exe567-proj1 \
+  --wandb_group arm_reach \
+  --exp_name ppo_arm_reach
---
+++ ece567_scripts/sac_arm_reach.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+XLA_PYTHON_CLIENT_MEM_FRACTION=.95 MUJOCO_GL=egl CUDA_VISIBLE_DEVICES=0 \
+uv run python /data/yhluo/ece567/JaxGCRL/run.py sac \
+  --env arm_reach \
+  --backend mjx \
+  --total_env_steps 10000000 \
+  --episode_length 1000 \
+  --num_envs 256 \
+  --num_eval_envs 256 \
+  --num_evals 50 \
+  --seed 1 \
+  --learning_rate 3e-4 \
+  --discounting 0.99 \
+  --batch_size 256 \
+  --unroll_length 62 \
+  --min_replay_size 1000 \
+  --max_replay_size 10000 \
+  --tau 0.005 \
+  --h_dim 256 \
+  --n_hidden 2 \
+  --normalize_observations \
+  --use_her \
+  --wandb_project_name exe567-proj1 \
+  --wandb_group arm_reach \
+  --exp_name sac_her_arm_reach
